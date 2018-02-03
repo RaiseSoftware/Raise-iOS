d86a82f72b2162db9545a230359701af5e71e548
Stephen Hayes <schayes4@gmail.com>
2018-02-03 15:15:14 -0500

    Add intro view controller

---
+++ Raise/UI/Intro/IntroViewController.swift
@@ -0,0 +1,16 @@
+//
+//  IntroViewController.swift
+//  Raise
+//
+//  Created by Stephen Hayes on 2/3/18.
+//  Copyright © 2018 Raise Software. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
