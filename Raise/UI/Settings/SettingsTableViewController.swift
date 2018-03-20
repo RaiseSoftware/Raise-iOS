5c5f97f31edd509e70e5532f21644c21d5525a51
Stephen Hayes <schayes4@gmail.com>
2018-03-20 19:10:11 -0400

    Updated asset structuring. Updated settings icon

---
+++ Raise/UI/Settings/SettingsTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsTableViewController.swift
+//  Raise
+//
+//  Created by Stephen Hayes on 3/20/18.
+//  Copyright © 2018 Raise Software. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.tableFooterView = UIView()
+    }
+}
